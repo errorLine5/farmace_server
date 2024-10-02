106d15a6117b5d8944c6181de449067d34d4c675
Shiberal <francesco.grelli98@gmail.com>
2024-10-02 17:04:31 +0200

    init_db.py

--- init_db.py
+++ init_db.py
@@ -1,8 +1,23 @@
+import os
 from Models.Users import Users
 from Models.Pharmacy import Pharmacy
 from Services.Database.dbsqlite import DBSqlite
 
-db = DBSqlite("database.db")
 
-db.executeRAW(Users.makeTableSqlite())
-db.executeRAW(Pharmacy.makeTableSqlite())
+sql_file = open("template_sqlite.sql", "r")
+database_file = "database.db"
+
+
+#delete database
+if os.path.exists(database_file):
+ os.remove(database_file)
+db = DBSqlite(database_file)
+
+
+commands = sql_file.read().split(";")
+for command in commands:
+ db.executeRAW(command)
+
+
+# db.executeRAW(Users.makeTableSqlite())
+# db.executeRAW(Pharmacy.makeTableSqlite())
