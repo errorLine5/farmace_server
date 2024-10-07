0ee93d38414a86447feea4d0e74f3026c6c40a0f
Sbelo2003 <beltramidanielebd@gmail.com>
2024-10-07 16:09:39 +0200

    fixed delete pharmacy

--- Controllers/pharmacy/delete.py
+++ Controllers/pharmacy/delete.py
@@ -7,11 +7,18 @@ class delete_pharmacy_ctl:
         self.auth = Auth(dbService)
 
     def delete_pharmacy(self, id, email, token):
-        self.auth.isAuth(email=email, token=token)
         
-        query = f"DELETE FROM pharmacy WHERE id = {id}"
-        self.dbService.delete(query, (id,))
-        if self.dbService.execute(query, (id,)):
-            return {"status": "success"}
-        else:
+        self.auth.isAuth(email=email, token=token)
+
+        query=f'SELECT * FROM pharmacy WHERE id = "{id}"'
+        
+        if len(self.dbService.selectRAW(query)) == 0:
             raise fastapi.HTTPException(status_code=404, detail="Pharmacy not found")
+        
+        try:
+            query = f'DELETE FROM pharmacy WHERE id = "{id}"'
+            self.dbService.execute(query)
+        except Exception as e:
+            return fastapi.HTTPException(status_code=404, detail="Error deleting pharmacy: " + str(e))
+        
+        return {"status": "success","deleted_pharmacy_id": id}
